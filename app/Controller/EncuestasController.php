752bfb6f9209975f9808019d4c36d27c417b94fd
lsi-frd-utn <lsifrdutn@gmail.com>
2015-02-06 19:55:54 -0300

    guardar preguntas de la encuesta

--- app/Controller/EncuestasController.php
+++ app/Controller/EncuestasController.php
@@ -21,41 +21,21 @@ class EncuestasController extends AppController {
 	        throw new NotFoundException(__('Estudiante Invalido'));
 	    }
 
+		if ($this->request->is(array('post', 'put'))) {
+			$this->Encuesta->id = $this->data['encuestaId'];
+			$this->Encuesta->set(array(
+			    'respuesta' => $this->data['respuesta']
+			));
+			$this->Encuesta->save();
+		}
+
+
 		$conditions = array(
-			'fields' => array('Encuesta.id','Encuesta.respuesta','Encuesta.legajo','Estudiante.id','Estudiante.nombre','Pregunta.pregunta'),
+			'order' => 'pregunta_id', //cambiar por orden
 			'conditions' => ['Estudiante.id' => $id]
 		);
 
-		$options = array(
-/*			'joins' => array(
-							array(
-								'table' => 'encuestas',
-							    'alias' => 'en',
-							    'type' => 'RIGHT',
-							    'conditions' => array('en.pregunta_id = Pregunta.id',
-							    	'en.estudiante_id'=>$id)
-							    )
-					)
-*/
-//			'conditions' => ['Estudiante.id' => $id]
-			'contain' => array('Encuesta'=>array('Estudiante'=>array('conditions'=>['Estudiante.id' => $id])) )
-		);
-
-/*
-SQL Query: SELECT `Encuesta`.`id`, `Encuesta`.`estudiante_id`, `Encuesta`.`pregunta_id`, `Encuesta`.`respuesta`, `Encuesta`.`legajo`, `Estudiante`.`id`, `Estudiante`.`legajo`, `Estudiante`.`nombre`, `Estudiante`.`carrera`, `Estudiante`.`tutor_id`, `Pregunta`.`id`, `Pregunta`.`pregunta`, `Pregunta`.`orden` FROM `tutorias`.`encuestas` AS `Encuesta` LEFT JOIN `tutorias`.`estudiantes` AS `Estudiante` ON (`Encuesta`.`estudiante_id` = `Estudiante`.`id`) LEFT JOIN `tutorias`.`preguntas` AS `Pregunta` ON (`Encuesta`.`pregunta_id` = `Pregunta`.`id`) RIGHT JOIN `tutorias`.`encuestas` AS `Encuesta` ON (`Encuesta`.`pregunta_id` = `Pregunta`.`id` AND `Encuesta`.`estudiante_id` = '1') WHERE 1 = 1
-*/
-
-		$respuestasDelEstudiante = $this->Encuesta->find('all', array('conditions' => ['Estudiante.id' => $id]));
-
-		$respuestasIds = Hash::extract( $respuestasDelEstudiante, '{n}.Pregunta.id');
-
-		$sinResponder = $this->Encuesta->Pregunta->find('all');
-
-		//debug($sinResponder);
-
-		$this->set('encuestas', $respuestasDelEstudiante);
-
-//		$this->set('encuestas', $this->Encuesta->Pregunta->find('all',$options));
+		$this->set('encuestas', $this->Encuesta->find('all', $conditions));
 
 	}
 }
--- app/View/Encuestas/index.ctp
+++ app/View/Encuestas/index.ctp
@@ -1,17 +1,16 @@
 <div class="encuestas ">
 	<h2><?php echo __('Encuesta de '); ?><?php echo $encuestas[0]['Estudiante']['nombre']; ?></h2>
 	<div>
-		<?php foreach ($encuestas as $encuesta): ?>
-			<?php echo $this->Form->create('Encuesta'); ?>
+	<?php foreach ($encuestas as $encuesta): ?>
+		<form action="/tutorias/encuestas/index/<?php echo $encuestas[0]['Estudiante']['id']; ?>" method='post'>
 			<fieldset>
-			<legend><?php echo h($encuesta['Pregunta']['pregunta']); ?></legend>
-			<?php
-				echo $this->Form->h($encuesta['Encuesta']['respuesta']);
-			?>
+				<legend><?php echo h($encuesta['Pregunta']['pregunta']); ?></legend>
+				<input type='hidden' name='encuestaId' id='encuestaId' value='<?php echo h($encuesta['Encuesta']['id']); ?>'/>
+				<input type='text'  name='respuesta' id='respuesta' value='<?php echo h($encuesta['Encuesta']['respuesta']); ?>'/>
+				<input type='submit'/>
 			</fieldset>
-			<?php echo $this->Form->end(__('Guardar')); ?>
-		<?php endforeach; ?>
-	<!-- p>< ?php debug($encuestas) ?></p -->
+		</form>
+	<?php endforeach; ?>
 	</div>
 
 </div>
