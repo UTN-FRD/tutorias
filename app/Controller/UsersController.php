7c05ad8774100b7284224de83cafc0bba2926aec
Matías Mangiantini <matiasm15@hotmail.com>
2016-05-16 22:41:15 -0300

    Soluciono un error que podría permitir a un ...
    ... tutor convertir su cuenta a administrador. Agrego una validación
    para que un usuario no pueda eliminarse a si mismo.

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -2,90 +2,100 @@
 App::uses('AppController', 'Controller');
 
 class UsersController extends AppController {
-    public function beforeFilter() {
-        parent::beforeFilter();
-        $this->Auth->allow('logout');
-    }
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow('logout');
+	}
 
-    public function index() {
-        $this->User->recursive = 0;
-        $this->set('users', $this->paginate());
-    }
+	public function index() {
+		$this->User->recursive = 0;
+		$this->set('users', $this->paginate());
+	}
 
-    public function add() {
-        if ($this->request->is('post')) {
-            $this->User->create();
-            if ($this->User->save($this->request->data)) {
-                $this->Session->setFlash('El usuario ha sido creado correctamente.', 'success');
-                return $this->redirect(array('action' => 'index'));
-            }
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->User->create();
+			if ($this->User->save($this->request->data)) {
+				$this->Session->setFlash('El usuario ha sido creado correctamente.', 'success');
+				return $this->redirect(array('action' => 'index'));
+			}
 
-            $this->Session->setFlash('No se ha podido crear el usuario. Por favor, intente nuevamente.', 'error');
-        }
-    }
+			$this->Session->setFlash('No se ha podido crear el usuario. Por favor, intente nuevamente.', 'error');
+		}
+	}
 
-    public function edit($id = null) {
-        $this->User->id = $id;
-        if (!$this->User->exists()) {
-            throw new NotFoundException(__('Usuario invalido'));
-        }
+	public function edit($id = null) {
+		$this->User->id = $id;
+		if (!$this->User->exists()) {
+			throw new NotFoundException(__('Usuario invalido'));
+		}
 
-        if ($this->request->is('post') || $this->request->is('put')) {
-            if ($this->User->save($this->request->data)) {
-                $this->Session->setFlash('El usuario ha sido actualizado correctamente.', 'success');
-                return $this->redirect(array('action' => 'index'));
-            }
+		if ($this->request->is('post') || $this->request->is('put')) {
+			unset($this->request->data['User']['role']);
 
-            $this->Session->setFlash('No se ha podido actualizar el usuario. Por favor, intente nuevamente.', 'error');
-        } else {
-            $this->request->data = $this->User->read(null, $id);
-            unset($this->request->data['User']['password']);
-        }
-    }
+			if ($this->User->save($this->request->data)) {
+				$this->Session->setFlash('El usuario ha sido actualizado correctamente.', 'success');
+				return $this->redirect(array('action' => 'index'));
+			}
 
-    public function delete($id = null) {
-        $this->request->allowMethod('post');
+			$this->Session->setFlash('No se ha podido actualizar el usuario. Por favor, intente nuevamente.', 'error');
+		} else {
+			$this->request->data = $this->User->read(null, $id);
+			unset($this->request->data['User']['password']);
+		}
+	}
 
-        $this->User->id = $id;
-        if (!$this->User->exists()) {
-            throw new NotFoundException(__('Usuario invalido'));
-        }
+	public function delete($id = null) {
+		$this->request->allowMethod('post');
 
-        if ($this->User->delete()) {
-            $this->Session->setFlash('El usuario ha sido eliminado correctamente.', 'success');
-        } else {
-            $this->Session->setFlash('No se ha podido eliminar el usuario. Por favor, intente nuevamente.', 'error');
-        }
+		$this->User->id = $id;
+		if (!$this->User->exists()) {
+			throw new NotFoundException(__('Usuario invalido'));
+		}
 
-        return $this->redirect(array('action' => 'index'));
-    }
+		if ($this->User->id == AuthComponent::user('id')) {
+			throw new ForbiddenException;
+		}
 
-    public function login() {
-        // If already logged-in, redirect
-        if($this->Auth->loggedIn()){
-            return $this->redirect($this->Auth->redirectUrl());
-        }
+		if ($this->User->delete()) {
+			$this->Session->setFlash('El usuario ha sido eliminado correctamente.', 'success');
+		} else {
+			$this->Session->setFlash('No se ha podido eliminar el usuario. Por favor, intente nuevamente.', 'error');
+		}
 
-        if ($this->request->is('post')) {
-            if ($this->Auth->login()) {
-                return $this->redirect($this->Auth->redirectUrl());
-            }
-            $this->Session->setFlash('Nombre de usuario o contraseña invalido. Por favor intente nuevamente o comuniquese con SAE.', 'error');
-        }
-    }
+		return $this->redirect(array('action' => 'index'));
+	}
 
-    public function logout() {
-        return $this->redirect($this->Auth->logout());
-    }
+	public function login() {
+		// If already logged-in, redirect
+		if($this->Auth->loggedIn()){
+			return $this->redirect($this->Auth->redirectUrl());
+		}
 
-    public function isAuthorized($user) {
-        if (in_array($this->action, array('edit'))) {
-            $userId = (int) $this->request->params['pass'][0];
-            if ($userId == $user['id']) {
-                return true;
-            }
-        }
+		if ($this->request->is('post')) {
+			if ($this->Auth->login()) {
+				return $this->redirect($this->Auth->redirectUrl());
+			}
+			$this->Session->setFlash('Nombre de usuario o contraseña invalido. Por favor intente nuevamente o comuniquese con SAE.', 'error');
+		}
+	}
 
-        return parent::isAuthorized($user);
-    }
+	public function logout() {
+		return $this->redirect($this->Auth->logout());
+	}
+
+	public function isAuthorized($user) {
+		if (in_array($this->action, array('check_username'))) {
+			return true;
+		}
+
+		if (in_array($this->action, array('edit'))) {
+			$userId = (int) $this->request->params['pass'][0];
+			if ($userId == $user['id']) {
+				return true;
+			}
+		}
+
+		return parent::isAuthorized($user);
+	}
 }
