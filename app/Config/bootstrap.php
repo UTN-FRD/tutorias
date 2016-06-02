948952f6d23b00b100b21dc201d01becb86c0eec
Jorge Alberto Cricelli <jacricelli@gmail.com>
2016-06-02 14:44:38 -0300

    Utilizar autocargador de clases de composer.

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -22,6 +22,17 @@
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
+/**
+ * Autocargador de clases de Composer
+ */
+require_once APP . DS . 'Vendor' . DS . 'autoload.php';
+/**
+ * Autocargador de clases de CakePHP
+ * https://github.com/composer/composer/commit/c80cb76b9b5082ecc3e5b53b1050f76bb27b127b
+ */
+spl_autoload_unregister(array('App', 'load'));
+spl_autoload_register(array('App', 'load'), true, true);
+
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File'));
 
