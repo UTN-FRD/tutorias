6a0cd22c595256b52b10ad83b21d567996c37550
Jorge Alberto Cricelli <jacricelli@gmail.com>
2016-06-03 23:45:42 -0300

    Actualizado enlace generado por la confirmación de eliminación.

--- app/webroot/js/index.js
+++ app/webroot/js/index.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     button = $(event.relatedTarget);
     pathArray = window.location.pathname.split( '/' );
 
-    $(this).find('form').attr('action', '/tutorias/' + pathArray[2] + '/delete/' + button.data('id'));
+    $(this).find('form').attr('action', window.baseUrl + pathArray[pathArray.length - 1] + '/delete/' + button.data('id'));
     $(this).find('.modal-body').text('¿Está seguro que desea borrar a ' + button.data('nombre') + '?');
-  })
+  });
 
   $('#btn-submit').click(function() {
     $(this).parents('form:first').submit();
   });
-})
+});
